98453525d69ebf8f63089bf6e38cc9a6def6d3ba
Dok6n <ehrbs2018@gmail.com>
2021-12-26 22:15:33 +0900

    feat: Add scale, translate image
    - 배경이미지 parallax scrolling 효과 적용
    - mouse move시 image translate 효과 적용
    - 안개 이미지 scale animation 적용

--- scroll/parallax-scrolling-sample02/js/script.js
+++ scroll/parallax-scrolling-sample02/js/script.js
@@ -6,16 +6,26 @@ let speed = 0.009;
 let scrollTop = 0;
 let progressBar;
 let imageWrap;
+let parallax_0;
+let parallax_1;
+let parallax_2;
+let parallax_3;
+let parallax_4;
+let parallax_5;
 
 window.onload = function () {
   progressBar = document.getElementsByClassName('progressBar')[0];
   imageWrap = document.getElementsByClassName('imageWrap')[0];
+  parallax_0 = document.getElementById('parallax_0');
+  parallax_1 = document.getElementById('parallax_1');
+  parallax_2 = document.getElementById('parallax_2');
+  parallax_3 = document.getElementById('parallax_3');
+  parallax_4 = document.getElementById('parallax_4');
+  parallax_5 = document.getElementById('parallax_5');
 
-  window.addEventListener('resize', stageResize, false);
   window.addEventListener('mousemove', mouseMove, false);
   window.addEventListener('scroll', scrollFunc, false);
 
-  stageResize();
   loop();
 };
 
@@ -24,13 +34,24 @@ function scrollFunc(e) {
   let per = Math.round((scrollTop / (document.body.scrollHeight - window.innerHeight)) * 100);
   progressBar.style.width = `${per}%`;
 
-  imageWrap.style.transform = `scale(${1 + scrollTop / 1000})`;
+  parallax_0.style.transform = `translate3d(0px, ${scrollTop * 0.03}px, 0px)`;
+  parallax_1.style.transform = `translate3d(0px, ${-scrollTop * 0.03}px, 0px)`;
+  parallax_2.style.transform = `translate3d(0px, ${-scrollTop * 0.12}px, 0px)`;
+  parallax_3.style.transform = `translate3d(0px, ${-scrollTop * 0.16}px, 0px)`;
 }
 
-function stageResize() {}
-
 function loop() {
+  mouseX += (x - mouseX) * speed;
+  mouseY += (y - mouseY) * speed;
+
+  parallax_4.style.transform = `translate3d(${mouseX / 140}px, ${-scrollTop * 0.22}px, 0px)`;
+  parallax_5.style.transform = `scale(1.1) translate(${mouseX / 50}px, ${-scrollTop * 0.25}px)`;
+  parallax_6.style.transform = `scale(1.2) translate(${-mouseX / 20}px, ${-mouseY / 20}px)`;
+
   window.requestAnimationFrame(loop);
 }
 
-function mouseMove(e) {}
+function mouseMove(e) {
+  x = e.clientX - window.innerWidth / 2;
+  y = e.clientY - window.innerWidth / 2;
+}
