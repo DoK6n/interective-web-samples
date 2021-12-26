664d6e279e8198005f2e80f924b6d55ccb154269
Dok6n <ehrbs2018@gmail.com>
2021-12-26 20:13:04 +0900

    feat: Add progress bar in parallax scroll sample02

--- scroll/parallax-scrolling-sample02/js/script.js
+++ scroll/parallax-scrolling-sample02/js/script.js
@@ -1,4 +1,14 @@
+let x = 0;
+let y = 0;
+let mouseX = 0;
+let mouseY = 0;
+let speed = 0.009;
+let scrollTop = 0;
+let progressBar;
+
 window.onload = function () {
+  progressBar = document.getElementsByClassName('progressBar')[0];
+
   window.addEventListener('resize', stageResize, false);
   window.addEventListener('mousemove', mouseMove, false);
   window.addEventListener('scroll', scrollFunc, false);
@@ -8,7 +18,9 @@ window.onload = function () {
 };
 
 function scrollFunc(e) {
-  let scrollTop = document.documentElement.scrollTop;
+  scrollTop = document.documentElement.scrollTop;
+  let per = Math.round((scrollTop / (document.body.scrollHeight - window.innerHeight)) * 100);
+  progressBar.style.width = `${per}%`;
 }
 
 function stageResize() {}
