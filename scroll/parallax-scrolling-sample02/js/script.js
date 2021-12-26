3459446b4d8633e3fdec03bafb51f1c9daccca3b
Dok6n <ehrbs2018@gmail.com>
2021-12-26 20:27:02 +0900

    feat: Add transform background scale

--- scroll/parallax-scrolling-sample02/js/script.js
+++ scroll/parallax-scrolling-sample02/js/script.js
@@ -5,9 +5,11 @@ let mouseY = 0;
 let speed = 0.009;
 let scrollTop = 0;
 let progressBar;
+let imageWrap;
 
 window.onload = function () {
   progressBar = document.getElementsByClassName('progressBar')[0];
+  imageWrap = document.getElementsByClassName('imageWrap')[0];
 
   window.addEventListener('resize', stageResize, false);
   window.addEventListener('mousemove', mouseMove, false);
@@ -21,6 +23,8 @@ function scrollFunc(e) {
   scrollTop = document.documentElement.scrollTop;
   let per = Math.round((scrollTop / (document.body.scrollHeight - window.innerHeight)) * 100);
   progressBar.style.width = `${per}%`;
+
+  imageWrap.style.transform = `scale(${1 + scrollTop / 1000})`;
 }
 
 function stageResize() {}
